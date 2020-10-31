0f1757151f87f9f641dd3d668272a2c8a1567e8b
dapp8coder <dapp.coder@protonmail.com>
2020-10-31 18:48:41 +0800

    add buyUniswapDAI.test.js

---
+++ test/buyUniswapDAI.test.js
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 Swap.Pet@pm.me
+// test/buyUniswapDAI.test.js 
+const { ethers } = require("ethers")
+const Ganache = require("ganache-core")
+const { abi } = require("swap.pet-lib/abi")
+console.log("abi:", abi)
+const { tokens } = require("swap.pet-lib/tokens")
+console.log("tokens:", tokens)
+
